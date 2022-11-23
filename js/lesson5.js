6aa68c02fe8ec33be4681418067f17a35bcbfc83
mubin-bonu <mubin.me@mail.ru>
2022-11-23 12:42:00 +0600

    dz6

--- js/lesson5.js
+++ js/lesson5.js
@@ -1,15 +1,11 @@
-document.querySelector("#generate").addEventListener("click",() => {
-    generate();
-});
 const click = document.querySelector("#click");
 const click2 = document.querySelector("#click2");
 
+
 let current = 0;
 
-generate = () => {
-    let quotes;
-    quotes = {
-        "- Уолт Дисней": '" Трудности преодолены."',
+let quotes = {
+    "- Уолт Дисней": '" Трудности преодолены."',
         "-Уинстон Чарчил": '" Я люблю Javascript"',
         "-Уил Рогерс": '" Учите Javascript"',
         "-Альберт Эйнштейн": '"Стремитесь не к успеху, а к ценностям, которые он дает"',
@@ -24,36 +20,54 @@ generate = () => {
         "-Лев Толстой": '"Каждый хочет изменить человечество, но никто не задумывается о том, как изменить себя."',
         "-Винсент Ван Гог": '"Если внутренний голос говорит вам, что вы не можете рисовать – рисуйте как можно больше, тогда этот голос затихнет."',
         "-Генри Форд": '"Неудача – это просто возможность начать снова, но уже более мудро."',
-    };
-    let authors = Object.keys(quotes);
-    // let author = authors[Math.floor(Math.random() * authors.length)];
-    // let quote = quotes[author];
-    let author = authors[current];
-    let quote = quotes[author];
-    document.querySelector("#quote").textContent = quote;
-    document.querySelector("#author").textContent = author;
 };
 
-click.addEventListener("click",() => {
-    current +=1;
-    if(current > 15) {
-        return false;
-    }
-    generate();
-});
-click2.addEventListener("click",() => {
-    current -=1;
-    if(current < 0) {
-        return false;
-    }
-    generate();
-});
 
-// document.querySelector("#click").addEventListener("click",() => {
-//     generate();
-// });
-// document.querySelector("#click2").addEventListener("click",() => {
-//     generate();
-// });
+const generate = () => {
+    let authors = Object.keys(quotes);
+    authors = authors[current];
+    let quote = quotes[authors];
+
+    document.querySelector("#quote").textContent = quote;
+    document.querySelector("#author").textContent = authors;
+
+}
+const generateRandom=()=>{
+    current = Math.floor(Math.random()*14)
+
+    let authors = Object.keys(quotes);
+    authors = authors[current];
+    console.log(authors)
+    let quote = quotes[authors];
+
+    document.querySelector("#quote").textContent = quote;
+    document.querySelector("#author").textContent = authors;
+}
+
+document.querySelector('#generate').addEventListener('click',generateRandom)
+
+
+click.addEventListener("click", ()=> {
+    current++;
+    if (current <= 15){
+        generate();
+    }else{
+        current=0
+        generate()
+    }
+});
+click2.addEventListener("click", () => {
+    current--;
+    if (current > 0){
+        generate();
+    }else{
+        current=14
+        generate()
+    }
+
+})
+
+
+
 
 
